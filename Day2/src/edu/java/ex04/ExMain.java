8cb4148edc347da6abb94cbfaa393b99a9c099fb
esn73 <esn7338@gmail.com>
2022-01-22 17:14:20 +0900

    .

--- Day2/src/edu/java/ex04/ExMain.java
+++ Day2/src/edu/java/ex04/ExMain.java
@@ -1,7 +1,10 @@
 package edu.java.ex04;
 
+import java.util.HashMap;
+import java.util.Map;
+
 // Java.lang 패키지의 클래스 이름들은 import 없이 사용가능
-// 같은 패키지의 클래스 이름들은 import 없이 사용 가능.
+// 같은 패키지의 클래스 이름들은 import 없이 사용 가능. time
 
 public class ExMain {
 	
@@ -23,19 +26,49 @@ public class ExMain {
 		double second = 31.5;
 		
 		Time t1 = new Time(); // 기본 생성자(default constructor) default생성자가 자동으로 만들어져있음 -> 생성자를 하나도 안만들어면 자동으로 생성
-		System.out.println(t1);
-		System.out.println(t1.hour); //참조연산자-변수이름으로 찾아감 
-		System.out.println(t1.minute);
-		System.out.println(t1.second);
+		System.out.println(t1);//println메서드는 객체의 toString() 메서드가 리턴하는 문자열을 콘솔에 출력함. t1객체가 자동으로 toString메서드를 호출
+//		System.out.println(t1.hour); //참조연산자-변수이름으로 찾아감 
+//		System.out.println(t1.minute);
+//		System.out.println(t1.second);
+		
+		System.out.println(t1.getHour());
+		System.out.println(t1.getMinute());
+		System.out.println(t1.getSecond());
+		
 		
 		Time t2 = new Time(14, 49, 30.0);// 파라미터를 갖는 생성자
 		//int %d, .3f-?소숫점 뒷자리 개수 \n 다음줄
-		System.out.printf("%d:%d:%.3f\n", t2.hour, t2.minute, t2.second);
+		
+		System.out.printf("%d:%d:%.3f\n",
+				t2.getHour(), t2.getMinute(), t2.getSecond());
+		//System.out.printf("%d:%d:%.3f\n", t2.hour, t2.minute, t2.second);
 //		String.format(format, args);
 		
 		Time t3 = new Time(3, 6, 0.0);
-		System.out.printf("%d:%d%.3f\n", t3.hour, t3.minute, t3.second);
+		System.out.printf("%d:%d:%.3f\n",
+				t3.getHour(), t3.getMinute(), t3.getSecond());
+//		System.out.printf("%d:%d%.3f\n", t3.hour, t3.minute, t3.second);
 		// %d: digit(10진수), %f: floating-point(실수), %s: String(문자열)
+		
+		System.out.println(t2.toString());
+		System.out.println(t3.toString());
+		
+		Time t4 = new Time(15, 50, 0.0);
+		Time t5 = new Time(15, 50, 0.0);
+		System.out.println(t4 == t5); // 주소를 호출해서 비교false -> equals
+		System.out.println(t4.equals(t5)); // 연산자를 호출해서 비교 -> true
+		
+		Map<String, String> map2 = new HashMap<>();
+		map2.put("a", "한");
+		map2.put("a", "글"); // 원소 1개 글로 덮어씌움
+		System.out.println(map2);
+		
+		
+		Map<Time, String> map = new HashMap<>();
+		map.put(t4, "adc");
+		map.put(t5, "def"); // 원소2개
+		System.out.println(map);
+		
 	}
 
 	
--- Day2/src/edu/java/ex04/Time.java
+++ Day2/src/edu/java/ex04/Time.java
@@ -1,18 +1,55 @@
 package edu.java.ex04;
 
-public class Time {
-
-	int hour;
-	int minute;
-	double second;
+public class Time { 
+	// 멤버 접근 수식어(access modifier): private < (package) < protected < public
+	private int hour;
+	private int minute;
+	private double second;
 	
 	// 생성자 오버로딩(overloading)
 	public Time( ) {}
+	
+	//Time( ) {} -> package
 	
 	public Time(int hour, int minute , double second) {
 		this.hour = hour; // this-> Heap의 확보된 메모리 공간
 		this.minute = minute;
 		this.second = second;
+	}
+	
+	// 메서드
+	public String toString() {
+		return String.format(
+				"%02d:%02d:%06.3f", 
+				this.hour, this.minute, this.second);
+	}
+	
+	public boolean equals(Object other) { //Time other 다형성
+		boolean result = false;
+		if(other instanceof Time) { // other가 Time 클래스의 인스턴스이면
+			Time t = (Time) other; // other를 Time 타입으로 강제 변환(casting)
+			result = (this.hour == t.hour) &&(this.minute == t.minute) &&(this.second == t.second);
+		} 		
+		
+		return result;
+	}
+	
+	public int hashCode() {
+		return Integer.valueOf(this.hour).hashCode()+
+			   Integer.valueOf(this.minute).hashCode()+
+			  Double.valueOf(this.second).hashCode();
+	}
+	
+	public int getHour() {
+		return hour; //this.hour
+	}
+	
+	public int getMinute() {
+		return minute;
+	}
+	
+	public double getSecond() {
+		return this.second;
 	}
 	
 }
